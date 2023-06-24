bda1163032434062592aaa3b2b631b4dd1beaa69
Mahri <k.myahri@gmail.com>
2023-06-25 00:07:32 +0500

    add all files

---
+++ js/main.js
@@ -0,0 +1,7 @@
+document.querySelector(".order__title").addEventListener("click", () => {
+  document.querySelector(".order").classList.toggle("order__open");
+});
+
+// document.querySelector(".product__detail").addEventListener("click", () => {
+//   document.querySelector(".modal").classList.toggle("modal__open");
+// });
